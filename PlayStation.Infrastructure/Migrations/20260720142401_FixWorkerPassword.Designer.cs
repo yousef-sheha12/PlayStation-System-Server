67557f836f4fe0c719691649da5e27180455f2bc
Yousef sheha <sheha8757@gmail.com>
2026-07-20 17:28:08 +0300

    Worker Edit

--- PlayStation.Infrastructure/Migrations/20260720142401_FixWorkerPassword.Designer.cs
+++ PlayStation.Infrastructure/Migrations/20260720142401_FixWorkerPassword.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlayStation.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using PlayStation.Infrastructure.Data;
 namespace PlayStation.Infrastructure.Migrations
 {
     [DbContext(typeof(PlayStationDbContext))]
-    partial class PlayStationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260720142401_FixWorkerPassword")]
+    partial class FixWorkerPassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PlayStation.Infrastructure/Migrations/20260720142401_FixWorkerPassword.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayStation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixWorkerPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT 1 FROM Users WHERE Email = 'worker@playstation.com')
+                    UPDATE Users SET PasswordHash = '$2a$11$9dOZzNTqZIyRJaZYvv6ULegnr.9DLazmOY6dS3Rr3UFhD3VVbu20e' WHERE Email = 'worker@playstation.com'
+                ELSE
+                    INSERT INTO Users (Email, PasswordHash, FullName, RoleId, IsActive, IsDeleted, CreatedAt)
+                    VALUES ('worker@playstation.com', '$2a$11$9dOZzNTqZIyRJaZYvv6ULegnr.9DLazmOY6dS3Rr3UFhD3VVbu20e', 'PlayStation Worker', 2, 1, 0, '2024-01-01T00:00:00.0000000Z');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Users WHERE Email = 'worker@playstation.com'");
+        }
+    }
+}
